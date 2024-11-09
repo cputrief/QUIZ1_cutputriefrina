2d393a6b7ee2ee6ccf3db9d8500b8dcd82713a70
cputrief <cutputriefrina@gmail.com>
2024-11-09 10:29:14 +0700

    membuat form dosen

---
+++ index.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- css link -->
+     <link rel="stylesheet" href="css/bootstrap.css">
+    <title>Quiz1</title>
+</head>
+<body>
+
+<div class="container">
+    <div class="row mt-5">
+        <div class="col-6 m-auto">
+            <div class="card">
+                <div class="card-header text-center">
+                    Formulir Dosen
+                </div>
+                <div class="card-body">
+                    <!-- atribut wajib di form -->
+                    <form action="proses_pert3.php" method="POST" enctype="multipart/form-data">
+                        <div class="mb-3">
+                            <label for="exampleInputEmail1" class="form-label">NIDN</label>
+                                <input type="text" name="nidn" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">      
+                        </div>
+                        <div class="mb-3">
+                            <label for="exampleInputPassword1" class="form-label">Nama Lengkap</label>
+                            <input type="text" name="nama" class="form-control" id="exampleInputPassword1">
+                        </div>
+                        <div class="mb-3">
+                            <label for="exampleInputPassword1" class="form-label">Rumpun</label>
+                            <select name="rumpun" id="" class="form-control">
+                                <option value="">-pilih Rumpun-</option>
+                                <option value="komputer">Komputer</option>
+                                <option value="akuntansi">Akuntansi</option>
+                                <option value="manajemen">Manajemen</option>
+                                <option value="perpajakan">Perpajakan</option>
+                                <option value="bisnis">Bisnis</option>
+                            </select>
+                        </div>
+                        <div class="mb-3">
+                            <label for="exampleInputPassword1" class="form-label">Tempat Lahir</label>
+                            <input type="text" name="tempat" class="form-control" id="exampleInputPassword1">
+                        </div>
+                        <div class="mb-3">
+                            <label for="exampleInputPassword1" class="form-label">Tanggal Lahir</label>
+                            <input type="date" name="tgl" class="form-control" id="exampleInputPassword1">
+                        </div>
+                        <div class="mb-3">
+                            <label for="exampleInputPassword1" class="form-label">Email</label>
+                            <input type="email" name="email" class="form-control" id="exampleInputPassword1">
+                        </div>
+                        <button type="submit" class="btn btn-primary">Submit</button>
+                        <button type="reset" class="btn btn-danger">Reset</button>
+                    </form>
+                </div>
+            </div>
+
+        </div>
+
+    </div>
+
+
+</div>
+    <script src="js/bootstrap.js"></script>
+    <script src="js/bootstrap.bundle.js"></script>
+</body>
+</html>
