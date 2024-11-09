9a0c91cb143ec96bbce561af9fda577c8c17a8a8
cputrief <cutputriefrina@gmail.com>
2024-11-09 10:33:01 +0700

    mengubah alamat action

--- index.php
+++ index.php
@@ -18,7 +18,7 @@
                 </div>
                 <div class="card-body">
                     <!-- atribut wajib di form -->
-                    <form action="proses_pert3.php" method="POST" enctype="multipart/form-data">
+                    <form action="index.php" method="POST" enctype="multipart/form-data">
                         <div class="mb-3">
                             <label for="exampleInputEmail1" class="form-label">NIDN</label>
                                 <input type="text" name="nidn" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">      
